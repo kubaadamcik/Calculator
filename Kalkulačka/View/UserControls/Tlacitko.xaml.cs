ccc9065c396fe39bc5f6f15aab0bbdc47eba6d4f
Jakub Adamčík <59259adamcik@student.sstebrno.cz>
2024-10-14 12:12:07 +0200

    Started making button

---
+++ Kalkulačka/View/UserControls/Tlacitko.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kalkulačka.View.UserControls
+{
+	/// <summary>
+	/// Interakční logika pro Tlacitko.xaml
+	/// </summary>
+	/// 
+	public partial class Tlacitko : UserControl
+	{
+		public Tlacitko()
+		{
+			InitializeComponent();
+		}
+
+		private int _num;
+
+		public int num
+		{
+			get { return _num; }
+			set { _num = value; }
+		}
+
+		private void btnCislo_Click(object sender, RoutedEventArgs e)
+		{
+
+        }
+    }
+}
