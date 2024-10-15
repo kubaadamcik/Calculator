e16151e5107bb79876b6baa763d971cff7992a1a
Jakub Adamčík <59259adamcik@student.sstebrno.cz>
2024-10-15 10:30:08 +0200

    made buttons

--- Kalkulačka/MainWindow.xaml.cs
+++ Kalkulačka/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.ComponentModel;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -14,11 +15,41 @@ namespace Kalkulačka
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window
+	public partial class MainWindow : Window, INotifyPropertyChanged
 	{
 		public MainWindow()
 		{
+			DataContext = this;
 			InitializeComponent();
+		}
+
+		private int _number;
+
+		public event PropertyChangedEventHandler? PropertyChanged;
+
+		public int Number
+		{
+			get { return _number; }
+			set 
+			{
+				_number = value;
+
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Number"));
+			}
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			Button? tlacitko = sender as Button;
+
+			if (tlacitko != null)
+			{
+			}
+		}
+
+		private static void AddNumber(int number)
+		{
+
 		}
 	}
 }
